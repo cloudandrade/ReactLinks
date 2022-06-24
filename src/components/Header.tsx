e0695e4ea04cf967456cf4810725c31cdfbd401e
Jan Andrade <cloud.andrade16@gmail.com>
2022-06-24 18:17:44 -0300

    part 2

---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+import { Logo } from './Logo'
+
+export function Header() {
+  return (
+    <header className='w-full py-5 flex items-center justify-center bg-gray-800 border-b border-gray-600'>
+      <Logo />
+    </header>)
+}
