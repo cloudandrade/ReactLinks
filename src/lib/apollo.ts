18468bdc666c53612c530a043275ded6887f4a05
Jan Andrade <cloud.andrade16@gmail.com>
2022-06-28 10:21:37 -0300

    remove libs unnused

--- src/lib/apollo.ts
+++
@@ -1,6 +0,0 @@
-import {ApolloClient, InMemoryCache}  from '@apollo/client'
-
-export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4rebw8d0zf301wgc22n2iri/master',
-  cache: new InMemoryCache()
-})
